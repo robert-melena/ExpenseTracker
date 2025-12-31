2566735eccdecf06a6065429deb62f785ef2c2f1
robert-melena <99871728+robert-melena@users.noreply.github.com>
2025-12-31 01:48:03 -0800

    Adding member variables to Expense Tracker

--- src/com/project/App.java
+++ src/com/project/App.java
@@ -1,7 +1,26 @@
 package com.project;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class App {
     public static void main(String[] args) {
 
+
+        LocalDate date = LocalDate.now();
+        System.out.println(date);
+        System.out.println(formattedDate());
+
     }
+
+    public static String formattedDate(){
+        LocalDateTime time = LocalDateTime.now();
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss");
+        return time.format(timeFormatter);
+    }
+
+
 }
+
+
--- src/com/project/Expense.java
+++ src/com/project/Expense.java
@@ -1,4 +1,22 @@
 package com.project;
 
+import java.time.LocalDateTime;
+
 public class Expense {
+
+    //declaring fields
+    //to hold date of transaction date and time
+    private LocalDateTime date;
+
+    //to hold category type and description
+    private String category;
+    private String description;
+    //amount for transaction
+    private int amount;
+    //payment method and notes
+    private String paymentMethod;
+    private String notes;
+
+
+
 }
